4266be335a0f2791d86b3bf2a0d7fce20b2f8e03
danya201272 <36302863+danya201272@users.noreply.github.com>
2023-07-17 00:37:41 +0300

    Add files via upload

--- vps_start.sh
+++ vps_start.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 VPNS=wg0 # Название интерфейса Wireguard
-SSH_PORT=22 # Порт SSH Изменять нельзя
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get -y install openssh-server
 sudo apt-get -y install curl
 sudo apt-get -y install bind9-utils
 sudo apt-get -y install cron
 sudo systemctl enable cron
+sudo systemctl enable sshd
 
-echo "Порт для SSH: ${SSH_PORT}" 
+
+read -p "Введите порт для SSH(22):" SSH_PORT
+
+read -p "Нужно ли отключение root и добавление нового пользователя для SSH(Y/N):" ROOOTS
+if [[ $ROOOTS == "y" || $ROOOTS == "Y" || $ROOOTS == "yes" || $ROOOTS == "Yes" || $ROOOTS == "Д" || $ROOOTS == "Да" || $ROOOTS == "д" || $ROOOTS == "да" ]]
+then
+	read -p "Введите имя нового пользователя(bino):" snames
+	sudo adduser $snames
+	echo "Это имя:${snames} и пароль нового пользователя ssh"
+	sudo sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
+	sudo sed -i "s|#Port 22|Port ${SSH_PORT}|g" /etc/ssh/sshd_config
+	sudo sed -i "18i PermitRootLogin no" /etc/ssh/sshd_config
+	sudo usermod -aG sudo $snames
+	sudo service ssh restart
+	echo "Успешно"
+else
+	sudo sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
+	sudo sed -i "s|#Port 22|Port ${SSH_PORT}|g" /etc/ssh/sshd_config
+	sudo sed -i "18i PermitRootLogin no" /etc/ssh/sshd_config
+	sudo service ssh restart
+fi
 
 sudo ip -br a
 read -p "Напиши название интерфейса VPS с доступом в интернет(выше ваши интерфейсы)(пример eth0,enp24s0):" WAN
@@ -31,25 +52,26 @@ sudo ./wireguard-install.sh
 
 read -p "Выше в консоли ищите Client WireGuard IPv4:(пример 10.66.66.2):" ip_vpn_client
 
-read -p "Выше в консоли ищите Server WireGuard port [1-65535]:(пример 60951):" WIREGUARD_PORT
+read -p "Выше в консоли ищите Server WireGuard port [1-65535]:(пример 50821):" WIREGUARD_PORT
 
 read -p "У вас серый ip с DDNS в локалке за туннелем(Y/N):" SERYI
 if [[ $SERYI == "y" || $SERYI == "Y" || $SERYI == "yes" || $SERYI == "Yes" || $SERYI == "Д" || $SERYI == "Да" || $SERYI == "д" || $SERYI == "да" ]]
 then
-	read -p "DDNS адрес пишите с NO-IP(пример hostesd.no-ip.com):" HOSTNAME
+	read -p "DDNS адрес пишите с NO-IP(пример hostesd.no-ip.com):" HOSTNAMESSSS
 	echo "Создаю скрипт обновления DDNS: ddns_update.sh"
 	sudo sed -i '/ddns_update.sh/d' /etc/crontab # Удаляет строку если нашла ddns_update.sh
 	sudo rm -f /usr/local/bin/ddns_update.sh
 	sudo curl -O https://raw.githubusercontent.com/danya201272/vps-ufw-wireguard-ports/main/ddns_update.sh
 	sudo chmod +x ddns_update.sh
-	sudo sed -i "2c HOSTNAME=${HOSTNAME} # С NO-IP или KeenDNS ip локального пк" ddns_update.sh
+	sudo sed -i "2c HOSTNAMESSSS=${HOSTNAMESSSS} # С NO-IP или KeenDNS ip локального пк" ddns_update.sh
 	sudo sed -i "3c WIREGUARD_PORT=${WIREGUARD_PORT} # WIREGUARD Порт" ddns_update.sh
+	sudo sed -i "4c SSH_PORT=${SSH_PORT} #  SSH Port" ddns_update.sh
 	sudo mv -f ddns_update.sh /usr/local/bin
 	sudo sed -i "19i */15 * * * * root /usr/local/bin/ddns_update.sh > /dev/null" /etc/crontab
 	echo "Скрипт ddns_update.sh в /usr/local/bin"
 	echo "Скрипт ddns_update.sh добавлен в /etc/crontab каждые 15 минут"
 else
-    read -p "Пишите IP Статику от провайдера(пример 176.213.115.169):" HOSTNAME
+    read -p "Пишите IP Статику от провайдера(пример 176.213.115.169):" HOSTNAMESSSS
 	:
 fi
 
@@ -75,9 +97,9 @@ else
 	:
 fi
 
-sudo ufw allow from $HOSTNAME to any port $SSH_PORT proto tcp
+sudo ufw allow from $HOSTNAMESSSS to any port $SSH_PORT proto tcp
 sudo ufw limit $SSH_PORT/tcp comment "SSH limit"
-sudo ufw allow from $HOSTNAME to any port $WIREGUARD_PORT proto udp
+sudo ufw allow from $HOSTNAMESSSS to any port $WIREGUARD_PORT proto udp
 sudo ufw allow in on $WAN to any port $GAME_TCP proto tcp comment "Public ip open to GAME_TCP_Port"
 sudo ufw allow in on $WAN to any port $GAME_UDP proto udp comment "Public ip open to GAME_UDP_Port"
 sudo ufw limit $GAME_TCP/tcp comment "GAME TCP Limit"
@@ -101,9 +123,26 @@ else
 	:
 fi
 
-sudo apt-get install fail2ban -y
-systemctl start fail2ban
-systemctl enable fail2ban
+read -p "Нужен ли fail2ban на SSH?(Y/N):" FAIL2TOBANSSS
+if [[ $FAIL2TOBANSSS == "y" || $FAIL2TOBANSSS == "Y" || $FAIL2TOBANSSS == "yes" || $FAIL2TOBANSSS == "Yes" || $FAIL2TOBANSSS == "Д" || $FAIL2TOBANSSS == "Да" || $FAIL2TOBANSSS == "д" || $FAIL2TOBANSSS == "да" ]]
+then
+	sudo apt-get install fail2ban -y
+	systemctl start fail2ban
+	systemctl enable fail2ban
+	sudo cp /etc/fail2ban/jail.{conf,local}
+	sudo sed -i '/#ignoreip/d' /etc/fail2ban/jail.local
+	sudo sed -i '/ignoreip =/d' /etc/fail2ban/jail.local
+	sudo sed -i "92i ignoreip = 127.0.0.1/8 ::1 ${ip_vpn_client}" /etc/fail2ban/jail.local
+	sudo sed -i "s|banaction = iptables-multiport|banaction = ufw|g" /etc/fail2ban/jail.local
+	sudo sed -i "s|banaction_allports = iptables-allports|banaction_allports = ufw|g" /etc/fail2ban/jail.local
+	sudo sed -i "s|port = ssh|port = ssh,sshd,${SSH_PORT}|g" /etc/fail2ban/jail.local
+	sudo sed -i "s|port    = ssh|port    = ssh,sshd,${SSH_PORT}|g" /etc/fail2ban/jail.local
+	sudo sed -i "s|port     = ssh|port     = ssh,sshd,${SSH_PORT}|g" /etc/fail2ban/jail.local
+	sudo systemctl restart fail2ban
+else
+	:
+fi
+
 
 
 sudo sed -i 's|net/ipv4/ip_forward=0|net/ipv4/ip_forward=1|g' /etc/ufw/sysctl.conf # IPV4
@@ -131,3 +170,22 @@ sudo service cron reload
 
 sudo ufw reload
 sudo ufw disable && sudo ufw enable
+
+echo "Порт SSH:${SSH_PORT}"
+echo "Порт Wireguard:${WIREGUARD_PORT}"
+echo "Порт Игровой TCP:${GAME_TCP}"
+echo "Порт Игровой UDP:${GAME_UDP}"
+echo "IP адрес клиента Wireguard:${ip_vpn_client}"
+echo "Ваш IP публичный адрес (сервера дома):${HOSTNAMESSSS}"
+echo "Имя нового пользователя:${snames}"
+
+read -p "Перезагрузить VPS?(Y/N)" VPSRESTARTSSS
+if [[ $VPSRESTARTSSS == "y" || $VPSRESTARTSSS == "Y" || $VPSRESTARTSSS == "yes" || $VPSRESTARTSSS == "Yes" || $VPSRESTARTSSS == "Д" || $VPSRESTARTSSS == "Да" || $VPSRESTARTSSS == "д" || $VPSRESTARTSSS == "да" ]]
+then
+	sudo reboot now
+	sudo reboot -f
+	sudo shutdown -r now
+	sudo systemctl reboot
+else
+	:
+fi
