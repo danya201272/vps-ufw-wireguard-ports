42a808a7ed52b8e064ea9641bb5c57efd7a9661f
danya201272 <36302863+danya201272@users.noreply.github.com>
2023-07-17 17:34:28 +0300

    Add files via upload

--- ddns_update.sh
+++ ddns_update.sh
@@ -3,11 +3,11 @@ HOSTNAMESSSS=vvvs.keenetick.pro # С NO-IP или KeenDNS ip локальног�
 WIREGUARD_PORT=50820 # WIREGUARD Порт
 SSH_PORT=22 #  SSH Port
 
-sudo ufw allow 53/tcp comment "DDNS script"
-sudo ufw allow 53/udp comment "DDNS script"
+/usr/sbin/ufw allow 53/tcp comment "DDNS script"
+/usr/sbin/ufw allow 53/udp comment "DDNS script"
 #IF IT DOES NOT WORK, AT LEAST ON UBUNTU INSTALL, bind-utils to get the host command
-
-#Create a cron */15 * * * * /usr/local/bin/ddns_update.sh > /dev/null
+#sudo crontab -e
+#Create a cron */15 * * * * root /usr/local/bin/ddns_update.sh > /dev/null
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
@@ -17,8 +17,8 @@ new_ip=$(host $HOSTNAMESSSS | head -n1 | cut -f4 -d ' ') #HOSTNAMESSSS=$(getent 
 old_ip=$(/usr/sbin/ufw status | grep $HOSTNAMESSSS | head -n1 | tr -s ' ' | cut -f3 -d ' ')
 if [ "$new_ip" = "$old_ip" ] ; then
     echo IP address has not changed
-	sudo ufw delete allow 53/tcp
-	sudo ufw delete allow 53/udp
+	/usr/sbin/ufw delete allow 53/tcp
+	/usr/sbin/ufw delete allow 53/udp
 else
     if [ -n "$old_ip" ] ; then
         /usr/sbin/ufw delete allow from $old_ip to any port $SSH_PORT proto tcp
@@ -27,7 +27,7 @@ else
     /usr/sbin/ufw allow from $new_ip to any port $SSH_PORT proto tcp comment $HOSTNAMESSSS
     /usr/sbin/ufw allow from $new_ip to any port $WIREGUARD_PORT proto udp comment $HOSTNAMESSSS
 	/usr/sbin/ufw limit $SSH_PORT/tcp
-	sudo ufw delete allow 53/tcp
-	sudo ufw delete allow 53/udp
+	/usr/sbin/ufw delete allow 53/tcp
+	/usr/sbin/ufw delete allow 53/udp
     echo UFW have been updated
 fi
